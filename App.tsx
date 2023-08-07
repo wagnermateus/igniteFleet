320889c0023b99c055002d0a3581fa5fb837874d
wagnermateus <wagnermateus7@gmail.com>
2023-08-07 19:28:26 +0100

    Estruruta do projeto - Fontes personalizadas + Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 import { ThemeProvider } from "styled-components";
-import { SignIn } from "./src/Screens/SignIn";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { SignIn } from "./src/screens/SignIn";
 import theme from "./src/theme";
+import { Loading } from "./src/components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
   <ThemeProvider theme={theme}>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
     <SignIn />
   </ThemeProvider>;
 }
