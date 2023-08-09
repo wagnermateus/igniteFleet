af9d74874ee03cc41fdb7013f6aee5ba623bb5e9
wagnermateus <wagnermateus7@gmail.com>
2023-08-09 20:54:39 +0100

    Interface de cadastro - Componente Button

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 import { TextAreaInput } from "../../components/TextAreaInput";
@@ -14,6 +15,8 @@ export function Departure() {
           label="Finalidade"
           placeholder="Vou utilizar o veículo para..."
         />
+
+        <Button title="Registar Saída" />
       </Content>
     </Container>
   );
