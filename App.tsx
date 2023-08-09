fa21652e8a9f06205ba1f86616b69b44971b0992
wagnermateus <wagnermateus7@gmail.com>
2023-08-09 20:39:08 +0100

    Interface de cadastro - Criando input para placa do veículo

--- App.tsx
+++ App.tsx
@@ -31,7 +31,9 @@ export default function App() {
   return (
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
-        <SafeAreaProvider style={{ backgroundColor: theme.COLORS.GRAY_800 }}>
+        <SafeAreaProvider
+          style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_800 }}
+        >
           <StatusBar
             barStyle="light-content"
             backgroundColor="transparent"
