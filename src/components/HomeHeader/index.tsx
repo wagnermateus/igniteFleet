bc3eed06bdbb896518ec1b662c489c87bebc39d5
wagnermateus <wagnermateus7@gmail.com>
2023-08-09 02:02:18 +0100

    Cloud Mongodb Atlas - Ícones

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,4 +1,8 @@
+import { TouchableOpacity } from "react-native";
 import { Container, Greeting, Message, Name } from "./styles";
+import { Power } from "phosphor-react-native";
+
+import theme from "../../theme";
 
 export function HomeHeader() {
   return (
@@ -8,6 +12,9 @@ export function HomeHeader() {
 
         <Name>Wagner</Name>
       </Greeting>
+      <TouchableOpacity>
+        <Power size={32} color={theme.COLORS.GRAY_400} />
+      </TouchableOpacity>
     </Container>
   );
 }
