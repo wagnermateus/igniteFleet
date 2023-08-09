d5beceebace2f1047edfa3f4da5fc3337382e191
wagnermateus <wagnermateus7@gmail.com>
2023-08-10 00:46:43 +0100

    Banco de Dados local - Fluxo da tela de registar chegada

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -14,7 +14,11 @@ export function Home() {
   const historic = useQuery(Historic);
 
   function handleRegisterMoviment() {
-    navigate("departure");
+    if (vehicleInUse?._id) {
+      navigate("arrival", { id: vehicleInUse._id.toString() });
+    } else {
+      navigate("departure");
+    }
   }
   function fetchVehicle() {
     try {
