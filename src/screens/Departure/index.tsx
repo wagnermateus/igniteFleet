0aff2d79f73a85463cd3f6c27af174df36ff12c7
wagnermateus <wagnermateus7@gmail.com>
2023-08-09 21:27:00 +0100

    Interface de cadastro - Input Refs

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,22 +1,40 @@
+import { useRef } from "react";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 import { TextAreaInput } from "../../components/TextAreaInput";
 import { Container, Content } from "./styles";
+import { TextInput } from "react-native";
 
 export function Departure() {
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    console.log("OK!");
+  }
   return (
     <Container>
       <Header title="Saída" />
       <Content>
-        <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
-
-        <TextAreaInput
-          label="Finalidade"
-          placeholder="Vou utilizar o veículo para..."
+        <LicensePlateInput
+          label="Placa do veículo"
+          placeholder="BRA1234"
+          onSubmitEditing={() => {
+            descriptionRef.current?.focus();
+          }}
+          returnKeyType="next"
         />
 
-        <Button title="Registar Saída" />
+        <TextAreaInput
+          ref={descriptionRef}
+          label="Finalizade"
+          placeholder="Vou utilizar o veículo para..."
+          onSubmitEditing={handleDepartureRegister}
+          returnKeyType="send"
+          blurOnSubmit
+        />
+
+        <Button title="Registar Saída" onPress={handleDepartureRegister} />
       </Content>
     </Container>
   );
