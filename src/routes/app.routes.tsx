aeee05a1f58542f7727e42feec1f5b9849887ecb
wagnermateus <wagnermateus7@gmail.com>
2023-08-10 00:23:54 +0100

    Banco de Dados local - Exibindo dados do banco

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Home } from "../screens/home";
+import { Home } from "../screens/Home";
 import { Departure } from "../screens/Departure";
 
 const { Navigator, Screen } = createNativeStackNavigator();
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,13 +2,24 @@ import { useNavigation } from "@react-navigation/native";
 import { CarStatus } from "../../components/CarStatus";
 import { HomeHeader } from "../../components/HomeHeader";
 import { Container, Content } from "./styles";
+import { useEffect } from "react";
+import { useQuery } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
 
 export function Home() {
   const { navigate } = useNavigation();
+  const historic = useQuery(Historic);
 
   function handleRegisterMoviment() {
     navigate("departure");
   }
+  function fetchVehicle() {
+    console.log(historic);
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
 
   return (
     <Container>
