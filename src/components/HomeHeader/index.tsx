0866dae1ce32a0a8324cded0d85c81e8b45208bd
wagnermateus <wagnermateus7@gmail.com>
2023-08-09 02:19:43 +0100

    Cloud Mongodb Atlas - Imagem do usuário

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,5 +1,5 @@
 import { TouchableOpacity } from "react-native";
-import { Container, Greeting, Message, Name } from "./styles";
+import { Container, Greeting, Message, Name, Picture } from "./styles";
 import { Power } from "phosphor-react-native";
 
 import theme from "../../theme";
@@ -7,6 +7,10 @@ import theme from "../../theme";
 export function HomeHeader() {
   return (
     <Container>
+      <Picture
+        source={{ uri: "https://github.com/wagnermateus.png" }}
+        placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
+      />
       <Greeting>
         <Message>Olá</Message>
 
