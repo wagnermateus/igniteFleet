7f42fcdaeabb2188af75efb41776b8062cf6122a
wagnermateus <wagnermateus7@gmail.com>
2023-08-09 20:47:58 +0100

    Interface de cadastro - Criando o TextAreaInput

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
+import { TextAreaInput } from "../../components/TextAreaInput";
 import { Container, Content } from "./styles";
 
 export function Departure() {
@@ -8,6 +9,11 @@ export function Departure() {
       <Header title="Saída" />
       <Content>
         <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
+
+        <TextAreaInput
+          label="Finalidade"
+          placeholder="Vou utilizar o veículo para..."
+        />
       </Content>
     </Container>
   );
